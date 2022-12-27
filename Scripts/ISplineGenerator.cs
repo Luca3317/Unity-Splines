1fd985feabcfc28e4196eafb8de25a5a695c326e
Luca3317 <lweist3317@gmail.com>
2022-12-27 02:18:22 +0100

    Revert "Added first implementation of ISplineGenerator" - Faulty commit
    This reverts commit 56255fefe185ed1be86ce6d5b45c0ebfd38e3f49.

--- Scripts/ISplineGenerator.cs
+++ Scripts/ISplineGenerator.cs
@@ -9,12 +9,7 @@ namespace UnitySplines
     */
     public interface ISplineGenerator
     {
-        public int SegmentSize { get; }
-        public int SlideSize { get; }
         public string GeneratorType { get; }
-
         public Vector3 Evaluate(float t, IList<Vector3> points);
-
-        protected const string _pointAmountErrorMessage = "The passed in amount of points ({0}) does not constitute exactly one {1} segment (required amount: {2})";
     }
 }
--- Scripts/Spline.cs
+++ Scripts/Spline.cs
@@ -12,43 +12,9 @@ namespace UnitySplines
     [System.Serializable]
     public class Spline
     {
-        public Spline(ISplineGenerator generator, params Vector3[] points) => Init(generator, points);
-        public Spline(ISplineGenerator generator, IEnumerable<Vector3> points) => Init(generator, points);
-        public Spline(ISplineGenerator generator, SplinePoints points) => Init(generator, points.Points);
-
-        public Vector3 ValueAt(float t)
-        {
-            (int segmentIndex, float segmentT) = PercentageToSegmentPercentage(t);
-            return Generator.Evaluate(segmentT, Points.Segment(segmentIndex));
-        }
-
-        public void SetGenerator(ISplineGenerator generator)
-        {
-            if (generator == Generator) return;
-            Generator = generator;
-            Points.SetSegmentSizes(Generator.SegmentSize, Generator.SlideSize);
-        }
+        public Vector3 ValueAt(float t) => throw new System.NotImplementedException();
 
         [SerializeField] protected SplinePoints Points;
         [SerializeField] protected ISplineGenerator Generator; 
-
-        protected void Init(ISplineGenerator generator, IEnumerable<Vector3> points)
-        {
-            Generator = generator;
-            Points = new SplinePoints(Generator.SegmentSize, Generator.SlideSize, points);
-        }
-
-        protected (int, float) PercentageToSegmentPercentage(float t)
-        {
-            int segmentIndex = (int)t;
-            if (t % 1 == 0)
-            {
-                segmentIndex--;
-                t = 1f;
-            }
-            else t %= 1;
-
-            return (segmentIndex, t);          
-        }
     }
 }
