5aa29f62f5e9d04a82a7a654d129161f072773d1
Luca3317 <lweist3317@gmail.com>
2023-01-07 14:06:29 +0100

    Reimplemented Beziers Spline generation to use matrix multiplication

--- Scripts/BezierGenerator.cs
+++ Scripts/BezierGenerator.cs
@@ -11,36 +11,39 @@ namespace UnitySplines.Bezier
 
         public string GeneratorType => _generatorType;
 
+        public Matrix4x4 CharacteristicMatrix => _characteristicMatrix;
+
         public static Vector3 Evaluate(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
         {
-            float t2 = t * t;
-            float t3 = t * t2;
-            float mt = 1 - t;
-            float mt2 = mt * mt;
-            float mt3 = mt * mt2;
-
-            // Bernstein Polynomial
-            // ValueAt_Cubic(t) =
-            // startPoint *      ( -t^3 + 3t^2 - 3t + 1 ) +
-            // controlPoint[0] * ( t3t^3 - 6t^2 + 3t ) +
-            // controlPoint[1] * ( -3t^3 + 3t^2 ) +
-            // endPoint *        ( t^3 )
-
-            return
-                p0 * mt3 +
-                p1 * 3 * mt2 * t +
-                p2 * 3 * mt * t2 +
-                p3 * t3;
+            return SplineHelper.Evaluate(t, 0, _characteristicMatrix, p0, p1, p2, p3);
         }
 
         public Vector3 Evaluate(float t, IList<Vector3> points)
         {
             if (points.Count != _segmentSize) throw new System.ArgumentException(string.Format(ISplineGenerator._pointAmountErrorMessage, points.Count, _generatorType, _segmentSize));
-            return Evaluate(t, points[0], points[1], points[2], points[3]);
+            return SplineHelper.Evaluate(t, 0, _characteristicMatrix, points);
+        }
+
+        public static Vector3 EvaluateDerivative(float t, int order, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+        {
+            return SplineHelper.Evaluate(t, order, _characteristicMatrix, p0, p1, p2, p3);
+        }
+
+        public Vector3 EvaluateDerivative(float t, int order, IList<Vector3> points)
+        {
+            if (points.Count != _segmentSize) throw new System.ArgumentException(string.Format(ISplineGenerator._pointAmountErrorMessage, points.Count, _generatorType, _segmentSize));
+            return SplineHelper.Evaluate(t, order, _characteristicMatrix, points);
         }
 
         private const int _segmentSize = 4;
         private const int _slideSize = 3;
         private const string _generatorType = "Bezier";
+        private static readonly Matrix4x4 _characteristicMatrix = new Matrix4x4
+        (
+            new Vector4(1, -3, 3, -1),
+            new Vector4(0, 3, -6, 3),
+            new Vector4(0, 0, 3, -3),
+            new Vector4(0, 0, 0, 1)
+        );
     }
 }
--- Scripts/ISplineGenerator.cs
+++ Scripts/ISplineGenerator.cs
@@ -13,7 +13,11 @@ namespace UnitySplines
         public int SlideSize { get; }
         public string GeneratorType { get; }
 
+        public Vector3 Evaluate(float t, params Vector3[] points) => Evaluate(t, (IList<Vector3>)points);
         public Vector3 Evaluate(float t, IList<Vector3> points);
+
+        public Vector3 EvaluateDerivative(float t, int order, params Vector3[] points) => EvaluateDerivative(t, order, (IList<Vector3>)points);
+        public Vector3 EvaluateDerivative(float t, int order, IList<Vector3> points);
 
         protected const string _pointAmountErrorMessage = "The passed in amount of points ({0}) does not constitute exactly one {1} segment (required amount: {2})";
     }
