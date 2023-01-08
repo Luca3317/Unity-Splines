4f03e16c32d80ce048cc67cc235eb8450fcf1615
Luca3317 <lweist3317@gmail.com>
2023-01-08 19:36:06 +0100

    Fixed Generators and helper class to work with SplinePoint objects

--- Scripts/ISplineGenerator.cs
+++ Scripts/ISplineGenerator.cs
@@ -13,12 +13,19 @@ namespace UnitySplines
         public int SlideSize { get; }
         public string GeneratorType { get; }
 
+        public Vector3 Evaluate<T>(float t, params T[] points) where T : SplinePointBase => Evaluate(t, SplineHelper.SplinePointsToVector(points));
+        public Vector3 Evaluate<T>(float t, IList<T> points) where T : SplinePointBase => Evaluate(t, SplineHelper.SplinePointsToVector(points));
         public Vector3 Evaluate(float t, params Vector3[] points) => Evaluate(t, (IList<Vector3>)points);
         public Vector3 Evaluate(float t, IList<Vector3> points);
 
+        public Vector3 EvaluateDerivative<T>(float t, int order, params T[] points) where T : SplinePointBase => EvaluateDerivative(t, order, SplineHelper.SplinePointsToVector(points));
+        public Vector3 EvaluateDerivative<T>(float t, int order, IList<T> points) where T : SplinePointBase => EvaluateDerivative(t, order, SplineHelper.SplinePointsToVector(points));
         public Vector3 EvaluateDerivative(float t, int order, params Vector3[] points) => EvaluateDerivative(t, order, (IList<Vector3>)points);
         public Vector3 EvaluateDerivative(float t, int order, IList<Vector3> points);
 
+        public IList<float> GetExtrema<T>(params T[] points) where T : SplinePointBase => GetExtrema(SplineHelper.SplinePointsToVector(points));
+        public IList<float> GetExtrema<T>(IList<T> points) where T : SplinePointBase => GetExtrema(SplineHelper.SplinePointsToVector(points));
+        public IList<float> GetExtrema(params Vector3[] points) => GetExtrema((IList<Vector3>)points);
         public IList<float> GetExtrema(IList<Vector3> points);
 
         protected const string _pointAmountErrorMessage = "The passed in amount of points ({0}) does not constitute exactly one {1} segment (required amount: {2})";
--- Scripts/SplineHelper.cs
+++ Scripts/SplineHelper.cs
@@ -2,102 +2,126 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class SplineHelper
+namespace UnitySplines
 {
-    /// <summary>
-    /// Converts a point index to the corresponding segment indeces.
-    /// </summary>
-    /// <param name="pointIndex">The point index that will be converted.</param>
-    /// <returns>The indeces of all segments that contain the point at i.</returns>
-    public static IEnumerable<int> PointToSegmentIndeces(int pointIndex, int segmentSize, int slideSize)
+    public static class SplineHelper
     {
-        List<int> indeces = new List<int>();
-
-        // Calculate index of first segment containing this point.
-        int index = pointIndex < segmentSize ? 0 : (pointIndex - segmentSize) / slideSize + 1;
-
-        while (pointIndex - index >= 0)
+        /// <summary>
+        /// Converts a point index to the corresponding segment indeces.
+        /// </summary>
+        /// <param name="pointIndex">The point index that will be converted.</param>
+        /// <returns>The indeces of all segments that contain the point at i.</returns>
+        public static IEnumerable<int> PointToSegmentIndeces(int pointIndex, int segmentSize, int slideSize)
         {
-            indeces.Add(index);
-            index += slideSize;
+            List<int> indeces = new List<int>();
+
+            // Calculate index of first segment containing this point.
+            int index = pointIndex < segmentSize ? 0 : (pointIndex - segmentSize) / slideSize + 1;
+
+            while (pointIndex - index >= 0)
+            {
+                indeces.Add(index);
+                index += slideSize;
+            }
+            return indeces;
         }
-        return indeces;
-    }
-    /// <summary>
-    /// Converts a segment index to the corresponding point index.
-    /// </summary>
-    /// <param name="segmentIndex">The segment index to convert.</param>
-    /// <returns>The index of the first point contained in this segment.</returns>
-    public static int SegmentToPointIndex(int segmentIndex, int segmentSize, int slideSize) => slideSize * segmentIndex;
+        /// <summary>
+        /// Converts a segment index to the corresponding point index.
+        /// </summary>
+        /// <param name="segmentIndex">The segment index to convert.</param>
+        /// <returns>The index of the first point contained in this segment.</returns>
+        public static int SegmentToPointIndex(int segmentIndex, int segmentSize, int slideSize) => slideSize * segmentIndex;
 
-    public static (int, float) PercentageToSegmentPercentage(float t)
-    {
-        int segmentIndex = (int)t;
-        if (t % 1 == 0 && t > 0)
+        public static IList<Vector3> SplinePointsToVector<T>(IList<T> points) where T : SplinePointBase
         {
-            segmentIndex--;
-            t = 1f;
-        }
-        else t %= 1;
-
-        return (segmentIndex, t);
-    }
-
-    public static Matrix4x4 CreateTMatrix(float t, int order)
-    {
-        float t2 = t * t;
-        float t3 = t2 * t;
-
-        Matrix4x4 tMatrix = new Matrix4x4();
-        switch (order)
-        {
-            case 0:
-                tMatrix[0, 0] = 1;
-                tMatrix[0, 1] = t;
-                tMatrix[0, 2] = t2;
-                tMatrix[0, 3] = t3;
-                break;
-
-            case 1:
-                tMatrix[0, 0] = 0;
-                tMatrix[0, 1] = 1;
-                tMatrix[0, 2] = 2 * t;
-                tMatrix[0, 3] = 3 * t2;
-                break;
-
-            case 2:
-                tMatrix[0, 0] = 0;
-                tMatrix[0, 1] = 0;
-                tMatrix[0, 2] = 2;
-                tMatrix[0, 3] = 6 * t;
-                break;
-
-            case 3:
-                tMatrix[0, 0] = 0;
-                tMatrix[0, 1] = 0;
-                tMatrix[0, 2] = 0;
-                tMatrix[0, 3] = 6;
-                break;
-
-            default: throw new System.NotImplementedException("");
+            List<Vector3> vectors = new List<Vector3>();
+            foreach (var item in points) vectors.Add(item.Position);
+            return vectors;
         }
 
-        return tMatrix;
+        public static (int, float) PercentageToSegmentPercentage(float t)
+        {
+            int segmentIndex = (int)t;
+            if (t % 1 == 0 && t > 0)
+            {
+                segmentIndex--;
+                t = 1f;
+            }
+            else t %= 1;
+
+            return (segmentIndex, t);
+        }
+
+        public static Matrix4x4 CreateTMatrix(float t, int order)
+        {
+            float t2 = t * t;
+            float t3 = t2 * t;
+
+            Matrix4x4 tMatrix = new Matrix4x4();
+            switch (order)
+            {
+                case 0:
+                    tMatrix[0, 0] = 1;
+                    tMatrix[0, 1] = t;
+                    tMatrix[0, 2] = t2;
+                    tMatrix[0, 3] = t3;
+                    break;
+
+                case 1:
+                    tMatrix[0, 0] = 0;
+                    tMatrix[0, 1] = 1;
+                    tMatrix[0, 2] = 2 * t;
+                    tMatrix[0, 3] = 3 * t2;
+                    break;
+
+                case 2:
+                    tMatrix[0, 0] = 0;
+                    tMatrix[0, 1] = 0;
+                    tMatrix[0, 2] = 2;
+                    tMatrix[0, 3] = 6 * t;
+                    break;
+
+                case 3:
+                    tMatrix[0, 0] = 0;
+                    tMatrix[0, 1] = 0;
+                    tMatrix[0, 2] = 0;
+                    tMatrix[0, 3] = 6;
+                    break;
+
+                default: throw new System.NotImplementedException("");
+            }
+
+            return tMatrix;
+        }
+
+        public static Matrix4x4 CreatePointMatrix(params Vector3[] points) => CreatePointMatrix((IList<Vector3>)points);
+        public static Matrix4x4 CreatePointMatrix(IList<Vector3> points)
+        {
+            if (points.Count > 4) throw new System.ArgumentException("");
+
+            Matrix4x4 pMatrix = new Matrix4x4();
+            for (int i = 0; i < points.Count; i++)
+                pMatrix.SetRow(i, points[i]);
+
+            return pMatrix;
+        }
+
+        public static Matrix4x4 CreatePointMatrix<T>(params T[] points) where T : SplinePointBase => CreatePointMatrix((IList<T>)points);
+        public static Matrix4x4 CreatePointMatrix<T>(IList<T> points) where T : SplinePointBase
+        {
+            if (points.Count > 4) throw new System.ArgumentException("");
+
+            Matrix4x4 pMatrix = new Matrix4x4();
+            for (int i = 0; i < points.Count; i++)
+                pMatrix.SetRow(i, points[i].Position);
+
+            return pMatrix;
+        }
+
+        public static Vector3 Evaluate(float t, int order, Matrix4x4 characteristicMatrix, Matrix4x4 pointMatrix) => (CreateTMatrix(t, order) * characteristicMatrix * pointMatrix).GetRow(0);
+        public static Vector3 Evaluate(float t, int order, Matrix4x4 characteristicMatrix, params Vector3[] points) => (CreateTMatrix(t, order) * characteristicMatrix * CreatePointMatrix(points)).GetRow(0);
+        public static Vector3 Evaluate(float t, int order, Matrix4x4 characteristicMatrix, IList<Vector3> points) => (CreateTMatrix(t, order) * characteristicMatrix * CreatePointMatrix(points)).GetRow(0);
+        public static Vector3 Evaluate<T>(float t, int order, Matrix4x4 characteristicMatrix, params T[] points) where T : SplinePointBase => (CreateTMatrix(t, order) * characteristicMatrix * CreatePointMatrix(points)).GetRow(0);
+        public static Vector3 Evaluate<T>(float t, int order, Matrix4x4 characteristicMatrix, IList<T> points) where T : SplinePointBase => (CreateTMatrix(t, order) * characteristicMatrix * CreatePointMatrix(points)).GetRow(0);
     }
-
-    public static Matrix4x4 CreatePointMatrix(params Vector3[] points) => CreatePointMatrix((IList<Vector3>)points);
-    public static Matrix4x4 CreatePointMatrix(IList<Vector3> points)
-    {
-        if (points.Count > 4) throw new System.ArgumentException("");
-
-        Matrix4x4 pMatrix = new Matrix4x4();
-        for (int i = 0; i < points.Count; i++)
-            pMatrix.SetRow(i, points[i]);
-
-        return pMatrix;
-    }
-
-    public static Vector3 Evaluate(float t, int order, Matrix4x4 characteristicMatrix, Matrix4x4 pointMatrix) => (CreateTMatrix(t, order) * characteristicMatrix * pointMatrix).GetRow(0);
-    public static Vector3 Evaluate(float t, int order, Matrix4x4 characteristicMatrix, params Vector3[] points) => (CreateTMatrix(t, order) * characteristicMatrix * CreatePointMatrix(points)).GetRow(0);
-    public static Vector3 Evaluate(float t, int order, Matrix4x4 characteristicMatrix, IList<Vector3> points) => (CreateTMatrix(t, order) * characteristicMatrix * CreatePointMatrix(points)).GetRow(0);
 }
