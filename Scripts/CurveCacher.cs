fab32f9a7f45f2ee7e1d5271dcaabfafb19ada8c
Luca3317 <lweist3317@gmail.com>
2023-01-10 17:32:53 +0100

    Minor changes to CurveCacher

--- Scripts/CurveCacher.cs
+++ Scripts/CurveCacher.cs
@@ -31,29 +31,32 @@ namespace UnitySplines
             get => _distances;
             set => _distances = value;
         }
-
-        public CurveCacher()
+        public IReadOnlyList<FrenetFrame> Frames
         {
-            _extrema = null;
-            _flattened = new List<Vector3>();
-            _length = -1;
-            _lengthAccuracy = -1;
-            _distances = new List<float>();
+            get => _frames;
+            set => _frames = value;
         }
+
+        public CurveCacher() => Clear();
 
         public void Clear()
         {
             _extrema = null;
-            _flattened = new List<Vector3>();
+            _flattened = null;
             _length = -1;
             _lengthAccuracy = -1;
-            _distances = new List<float>();
+            _distances = null;
+            _frames = null;
         }
 
+        // TODO
+        // Make the Ireadonlylists ienumerables?
+        // SplineCacher could iterate over its curvecachers and yield them
         private SplineExtrema? _extrema;
         private IReadOnlyList<Vector3> _flattened;
         private float _length;
         private int _lengthAccuracy;
         private IReadOnlyList<float> _distances;
+        private IReadOnlyList<FrenetFrame> _frames;
     }
 }
